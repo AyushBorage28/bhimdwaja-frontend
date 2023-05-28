cd067ea48f91547c876a5fe72735a97864bec6d8
Ayush Borage <ayushborage28@gmail.com>
2023-05-28 23:33:30 +0530

    final faqs

--- src/screens/FAQsScreen.js
+++ src/screens/FAQsScreen.js
@@ -1,88 +1,99 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Accordion from 'react-bootstrap/Accordion'
-import { Link } from 'react-router-dom'
 
 const FAQsScreen = () => {
     return (
         <>
             <h1 className='text-center pt-4'>Frequently Asked Questions</h1>
 
-            <div className='container p-3 faqs-container' style={{color: 'black'}}>
-            <Accordion defaultActiveKey="0">
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="0">
-                    What is rudraksha?
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>Rudraksha is a seed that comes from the Elaeocarpus ganitrus tree, which is found in the Himalayan region. It is considered sacred in Hinduism and is often used in prayer and meditation.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="1">
-                    What are the benefits of wearing rudraksha? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body>Wearing rudraksha is believed to have many benefits, including increasing concentration, reducing stress and anxiety, improving blood circulation, and providing overall physical and mental well-being.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="2">
-                    What are gems?
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="2">
-                        <Card.Body>Gems are natural minerals or stones that are prized for their beauty and rarity. They are often used in jewellery and are believed to have healing and therapeutic properties.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="3">
-                    Can I wear rudraksha and gems together? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="3">
-                        <Card.Body>Yes, you can wear rudraksha and gems together. In fact, many people believe that wearing a combination of rudraksha and gems can enhance their benefits.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="4">
-                    What is a rudraksha mala? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="4">
-                        <Card.Body>A rudraksha mala is a string of rudraksha beads that is used for prayer and meditation. It is often worn around the neck or wrist.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="5">
-                    How do I know what size of rudraksha or gem to buy? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="5">
-                        <Card.Body>The size of rudraksha or gem you need depends on your personal preference and the purpose for which you are buying it. We provide detailed information about the size and properties of each of our products on our website to help you make an informed decision.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="6">
-                    How do I take care of my rudraksha or gem? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="6">
-                        <Card.Body>To take care of your rudraksha or gem, avoid exposing it to harsh chemicals or extreme temperatures. You can clean it with a soft cloth and mild soap or use a specialized cleaning solution for gems. Store it in a dry and safe place when not in use.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="7">
-                    How long does shipping take? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="7">
-                        <Card.Body>We ship our products within three business days of receiving payment. Shipping times vary depending on your location, but typically take between 10-14 business days.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="8">
-                    Can I cancel my order? 
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="8">
-                        <Card.Body>If you need to cancel your order, please contact us as soon as possible. If your order has not yet shipped, we will process a cancellation and refund. However, if your order has already shipped, we are unable to cancel it.</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
+            <div className='container p-3 faqs-container' style={{ color: 'black' }}>
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                            What is Rudraksha?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body className='faqs-body-text'>Rudraksha, derived from the Elaeocarpus ganitrus tree, not only possesses spiritual significance but also holds a rich historical and cultural legacy. These mystical seeds are primarily found in the serene and majestic Himalayan region, known for its breathtaking landscapes and spiritual retreats.
+
+                                The association of Rudraksha with Hinduism dates back thousands of years, and it has played a pivotal role in various religious practices. In ancient texts, such as the Puranas and Upanishads, Rudraksha is mentioned as a divine gift bestowed upon humanity by Lord Shiva himself.
+
+                                Rudraksha is not merely a seed but a powerful symbol of spirituality, devotion, and holistic well-being. Its origins in the Himalayan region, sacred significance in Hinduism, and diverse benefits make it a cherished and revered element in the lives of millions of people around the world.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="1">
+                            What are the benefits of wearing Rudraksha?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body className='faqs-body-text'>Wearing Rudraksha beads has been attributed to a multitude of benefits that extend beyond their spiritual significance. These mystical seeds have gained popularity for their potential positive impact on various aspects of an individual's well-being. One of the key advantages associated with Rudraksha is its ability to enhance concentration and focus. Many practitioners believe that wearing Rudraksha beads close to the skin stimulates the mind and promotes clarity of thought, making it a favored accessory during meditation and study sessions.
+
+                                In addition to its mental and emotional benefits, Rudraksha is also believed to positively impact physical health. The beads are thought to improve blood circulation, which plays a vital role in maintaining overall well-being. Furthermore, the beads are considered to have a rejuvenating effect on the body, potentially boosting vitality and energy levels.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="2">
+                            What are Gems?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body className='faqs-body-text'>Gems, those exquisite natural minerals and stones renowned for their captivating beauty, have captivated humanity for centuries. These precious treasures, found deep within the Earth's crust, hold immense value not only for their aesthetic allure but also for their perceived healing and therapeutic properties.
+
+                                Gemstones have been cherished and utilized in jewelry-making across various cultures and civilizations throughout history. Their allure lies in their vibrant colors, unique patterns, and remarkable optical properties that create an enchanting play of light. From the mesmerizing blues of sapphires to the fiery reds of rubies, the kaleidoscope of gemstone hues offers an endless array of options to adorn oneself with elegance and style.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="3">
+                            Can I wear Rudraksha and Gems together?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="3">
+                            <Card.Body className='faqs-body-text'>Yes, you can wear Rudraksha and gems together. In fact, many people believe that wearing a combination of Rudraksha and gems can enhance their benefits.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="4">
+                            What is a Rudraksha Mala?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="4">
+                            <Card.Body className='faqs-body-text'>A Rudraksha Mala holds a significant place in spiritual practices, serving as a powerful tool for prayer and meditation. Consisting of a string of Rudraksha beads, this sacred accessory is commonly worn around the neck or wrapped around the wrist, symbolizing devotion and spiritual connection.
+
+                                The Rudraksha beads strung together on the mala are carefully selected based on their mukhi or face pattern, each associated with a specific deity or energy. The number of beads in a Rudraksha mala can vary, with commonly used counts being 108 or 54 beads, although other numbers are also prevalent.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="5">
+                            How do I know what size of Rudraksha or Gem to buy?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="5">
+                            <Card.Body className='faqs-body-text'>The size of Rudraksha or gem you need depends on your personal preference and the purpose for which you are buying it. We provide detailed information about the size and properties of each of our products on our website to help you make an informed decision.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="6">
+                            How do I take care of my Rudraksha or Gems?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="6">
+                            <Card.Body className='faqs-body-text'>To take care of your Rudraksha or gem, avoid exposing it to harsh chemicals or extreme temperatures. You can clean it with a soft cloth and mild soap or use a specialized cleaning solution for gems. Store it in a dry and safe place when not in use.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="7">
+                            How long does shipping take?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="7">
+                            <Card.Body className='faqs-body-text'>We ship our products within three business days of receiving payment. Shipping times vary depending on your location, but typically take between 10-14 business days.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="8">
+                            Can I cancel my order?
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="8">
+                            <Card.Body className='faqs-body-text'>If you wish to cancel your order, we kindly request you to reach out to us as soon as you can.
+
+                                If your order has not yet been shipped, we will make every effort to process your cancellation request promptly. In such cases, we will initiate the cancellation process and arrange for a refund of the payment you made for the order.</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
             </div>
         </>
     )
