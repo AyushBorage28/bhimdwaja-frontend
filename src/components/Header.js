4562cfe9716e1f0932941f9c09536429eb7ec78a
Ayush Borage <ayushborage28@gmail.com>
2023-06-05 16:42:27 +0530

    minor edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Route, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
