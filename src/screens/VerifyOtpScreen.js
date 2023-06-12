91a3ebb00a770d6f12f9a03d2bb059a9eb54526f
ayushborage28 <ayushborage28@gmail.com>
2023-06-12 22:50:50 +0530

    add 60s timer

--- src/screens/VerifyOtpScreen.js
+++ src/screens/VerifyOtpScreen.js
@@ -1,46 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Loader from '../components/Loader'
-import axios from 'axios'
+import Loader from '../components/Loader';
+import axios from 'axios';
 import { SERVER_URL } from '../config';
-import Message from '../components/Message'
+import Message from '../components/Message';
 import { useDispatch, useSelector } from 'react-redux';
 import { USER_VERIFIED } from '../constants/userConstants';
 import { findUser, verifyUser } from '../actions/userActions';
 
 function Otpinput({ history, location }) {
-  const [otp1, setOtp1] = useState("");
-  const [otp2, setOtp2] = useState("");
-  const [otp3, setOtp3] = useState("");
-  const [otp4, setOtp4] = useState("");
-  const [otp5, setOtp5] = useState("");
-  const [otp6, setOtp6] = useState("");
-  const [message, setMessage] = useState(null)
-  // const [error, setError] = useState("");
-  const [phone, setPhone] = useState();
+  const [otp1, setOtp1] = useState('');
+  const [otp2, setOtp2] = useState('');
+  const [otp3, setOtp3] = useState('');
+  const [otp4, setOtp4] = useState('');
+  const [message, setMessage] = useState(null);
+  const [phone, setPhone] = useState('');
+  const [timer, setTimer] = useState(60);
 
   const dispatch = useDispatch();
-  const userLogin = useSelector((state) => state.userLogin)
-  const { loading, error, userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
 
-  const redirect = location.search ? location.search.split('=')[1] : '/'
+  const redirect = location.search ? location.search.split('=')[1] : '/';
 
   useEffect(() => {
     if (
       !location ||
       !location.search ||
-      !location.search.split("phone=")[1] ||
-      !location.search.split("phone=")[1].split("&")[0]
+      !location.search.split('phone=')[1] ||
+      !location.search.split('phone=')[1].split('&')[0]
     ) {
       // TODO: throw some error
     }
     if (userInfo) {
-      history.push(redirect)
-      window.location.reload()
+      history.push(redirect);
+      window.location.reload();
     }
 
-    const _phone = location.search.split("phone=")[1].split("&")[0];
-    setPhone(_phone)
+    const _phone = location.search.split('phone=')[1].split('&')[0];
+    setPhone(_phone);
   }, [history, userInfo, redirect]);
 
   async function handleSubmit(event) {
@@ -48,58 +46,70 @@ function Otpinput({ history, location }) {
     const otp = otp1 + otp2 + otp3 + otp4;
     dispatch(verifyUser(phone, otp, () => {
       history.push(redirect);
-      window.location.reload()
-    }))
+      window.location.reload();
+    }));
   }
 
   const handleResendOTP = async () => {
     try {
-      const { data } = await axios.post(`${SERVER_URL}/api/users/resendOTP`, { phone })
-      setMessage(data.message)
+      setTimer(60);
+      const { data } = await axios.post(`${SERVER_URL}/api/users/resendOTP`, { phone });
+      setMessage(data.message);
       setTimeout(() => {
         setMessage(null);
       }, 1000);
+    } catch (err) {
+      // Handle the error if the OTP resend fails
     }
-    catch (err) {
-    }
-  }
+  };
 
   const inputfocus = (elmnt) => {
-    if (elmnt.key === "Delete" || elmnt.key === "Backspace") {
+    if (elmnt.key === 'Delete' || elmnt.key === 'Backspace') {
       const next = elmnt.target.tabIndex - 2;
       if (next > -1) {
-
-        elmnt.target.form.elements[next].focus()
+        elmnt.target.form.elements[next].focus();
       }
-    }
-    else {
-      console.log("next");
-
+    } else {
+      console.log('next');
       const next = elmnt.target.tabIndex;
       if (next < 7) {
-        elmnt.target.form.elements[next].focus()
+        elmnt.target.form.elements[next].focus();
       }
     }
+  };
 
-  }
+  useEffect(() => {
+    let interval;
+
+    if (timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [timer]);
 
   return (
-    <><h1 className='text-center pt-4'>Verify OTP</h1>
-      <form onSubmit={handleSubmit} className='otp-form'>
-        {error && <Message variant='danger'>{error}</Message>}
-        {message && <Message variant='danger'>{message}</Message>}
+    <>
+      <h1 className="text-center pt-4">Verify OTP</h1>
+      <form onSubmit={handleSubmit} className="otp-form">
+        {error && <Message variant="danger">{error}</Message>}
+        {message && <Message variant="danger">{message}</Message>}
         {loading && <Loader />}
 
         <div className="otpContainer">
-
           <input
             name="otp1"
             type="text"
             autoComplete="off"
             className="otpInput"
             value={otp1}
-            onChange={e => setOtp1(e.target.value)}
-            tabIndex="1" maxLength="1" onKeyUp={e => inputfocus(e)} />
+            onChange={(e) => setOtp1(e.target.value)}
+            tabIndex="1"
+            maxLength="1"
+            onKeyUp={(e) => inputfocus(e)}
+          />
           <input
             name="otp2"
             type="text"
@@ -124,20 +134,26 @@ function Otpinput({ history, location }) {
             value={otp4}
             onChange={e => setOtp4(e.target.value)}
             tabIndex="4" maxLength="1" onKeyUp={e => inputfocus(e)} />
-
         </div>
-        <p>If didn't receive in 60 seconds, click on Resend OTP.</p>
+        <p>
+          {timer > 0 ? (
+            `If you didn't receive the OTP in ${timer} seconds, click on Resend OTP.`
+          ) : (
+            'If you didnt receive the OTP, click on Resend OTP.'
+          )}
+        </p>
+
         <div className="otp-buttons">
           <Button className="primary otp-btn" type="submit">
             Submit
           </Button>
-          <Button className="primary otp-btn" onClick={handleResendOTP}>
+          <Button className="primary otp-btn" onClick={handleResendOTP} disabled={timer > 0}>
             Resend OTP
           </Button>
         </div>
-      </form></>
+      </form>
+    </>
   );
 }
-
 
 export default Otpinput;
