b4da16fda248799af8787a6b951aa79544d55a7c
ayushborage28 <ayushborage28@gmail.com>
2023-06-14 00:08:22 +0530

    add logo loader

--- src/App.js
+++ src/App.js
@@ -31,11 +31,13 @@ import PrivacyPolicyScreen from "./screens/PrivacyPolicyScreen";
 import SimpleMap from "./screens/ContactUs";
 import ShippingAndReturn from "./screens/ShippingAndReturn";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import LogoLoader from "./components/LogoLoader";
 
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [scrollProgress, setScrollProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -47,10 +49,18 @@ const App = () => {
       setScrollProgress(progress);
     };
 
+    const handleLoad = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("load", handleLoad);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("load", handleLoad);
     };
   }, []);
 
@@ -58,63 +68,68 @@ const App = () => {
   return (
     <CategoryContext.Provider value={{ selectedCategory, setSelectedCategory }}>
       <Router>
-      <div style={{ position: "relative", minHeight: "100vh" }}>
-          
-            <Header />
-            <div style={{ paddingBottom: "60px" }}>
-              <Switch>
-                <Route path="/order/:id" component={OrderScreen} />
-                <Route path="/shipping" component={ShippingScreen} />
-                <Route path="/payment" component={PaymentScreen} />
-                <Route path="/placeorder" component={PlaceOrderScreen} />
-                <Route path="/login" component={LoginScreen} />
-                <Route path="/category/:category" component={ProductPage} />
-                <Route path="/categories" component={CategoryPage} />
-                <Route path="/register" component={RegisterScreen} />
-                <Route path="/verifyotp" component={VerifyOtpScreen} />
-                <Route path="/terms-and-conditions" component={TandCScreen} />
-                <Route path="/faqs" component={FAQsScreen} />
-                <Route path="/customization" component={CustomizationScreen} />
-                <Route path="/contact-us" component={SimpleMap} />
-                <Route path="/shipping-and-return" component={ShippingAndReturn} />
-                <Route path="/energization" component={EnergizationScreen} />
-                <Route path="/privacy-policy" component={PrivacyPolicyScreen} />
-                <Route path="/profile" component={ProfileScreen} />
-                <Route path="/product/:id" component={ProductScreen} />
-                <Route path="/cart/:id?" component={CartScreen} />
-                <Route path="/admin/userlist" component={UserListScreen} />
-                <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-                <Route
-                  path="/admin/productlist"
-                  component={ProductListScreen}
-                  exact
-                />
-                <Route
-                  path="/admin/productlist/:pageNumber"
-                  component={ProductListScreen}
-                  exact
-                />
-                <Route
-                  path="/admin/product/:id/edit"
-                  component={ProductEditScreen}
-                />
-                <Route path="/admin/orderlist" component={OrderListScreen} />
-                <Route path="/search/:keyword" component={SearchScreen} exact />
+        <div style={{ position: "relative", minHeight: "100vh" }}>
+          {isLoading ? (
+            <LogoLoader />
+          ) : (
+            <>
+              <Header />
+              <div style={{ paddingBottom: "60px" }}>
+                <Switch>
+                  <Route path="/order/:id" component={OrderScreen} />
+                  <Route path="/shipping" component={ShippingScreen} />
+                  <Route path="/payment" component={PaymentScreen} />
+                  <Route path="/placeorder" component={PlaceOrderScreen} />
+                  <Route path="/login" component={LoginScreen} />
+                  <Route path="/category/:category" component={ProductPage} />
+                  <Route path="/categories" component={CategoryPage} />
+                  <Route path="/register" component={RegisterScreen} />
+                  <Route path="/verifyotp" component={VerifyOtpScreen} />
+                  <Route path="/terms-and-conditions" component={TandCScreen} />
+                  <Route path="/faqs" component={FAQsScreen} />
+                  <Route path="/customization" component={CustomizationScreen} />
+                  <Route path="/contact-us" component={SimpleMap} />
+                  <Route path="/shipping-and-return" component={ShippingAndReturn} />
+                  <Route path="/energization" component={EnergizationScreen} />
+                  <Route path="/privacy-policy" component={PrivacyPolicyScreen} />
+                  <Route path="/profile" component={ProfileScreen} />
+                  <Route path="/product/:id" component={ProductScreen} />
+                  <Route path="/cart/:id?" component={CartScreen} />
+                  <Route path="/admin/userlist" component={UserListScreen} />
+                  <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+                  <Route
+                    path="/admin/productlist"
+                    component={ProductListScreen}
+                    exact
+                  />
+                  <Route
+                    path="/admin/productlist/:pageNumber"
+                    component={ProductListScreen}
+                    exact
+                  />
+                  <Route
+                    path="/admin/product/:id/edit"
+                    component={ProductEditScreen}
+                  />
+                  <Route path="/admin/orderlist" component={OrderListScreen} />
+                  <Route path="/search/:keyword" component={SearchScreen} exact />
 
-                {/* <Route
+                  {/* <Route
                 path="/search/:keyword/page/:pageNumber"
                 component={HomeScreen}
                 exact
               /> */}
-                <Route path="/page/:pageNumber" component={HomeScreen} exact />
-                <Route path="/" component={HomeScreen} exact />
-                <Route component={PageNotFound} />
-              </Switch>
-              <ProgressBar now={scrollProgress} style={{ width: '100%', height: '2px', backgroundColor: '#b27b23' }} className="fixed-top" variant="warning" />
-              <div style={{ marginTop: '2px' }}></div>
-            </div>
-          </div>
-          <Footer style={{ position: "absolute", bottom: 0, width: "100%" }} />
+                  <Route path="/page/:pageNumber" component={HomeScreen} exact />
+                  <Route path="/" component={HomeScreen} exact />
+                  <Route component={PageNotFound} />
+                </Switch>
+                <ProgressBar now={scrollProgress} style={{ width: '100%', height: '2px', backgroundColor: '#b27b23' }} className="fixed-top" variant="warning" />
+                <div style={{ marginTop: '2px' }}></div>
+              </div>
+            </>
+          )}
+        </div>
+        <Footer style={{ position: "absolute", bottom: 0, width: "100%" }} />
       </Router>
     </CategoryContext.Provider>
   );
---
+++ src/components/LogoLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogoLoader = () => {
+    return (
+        <div className="logo-loader">
+            <img src='./images/logo-loader.png' alt="Loader" />
+        </div>
+    );
+};
+
+export default LogoLoader;
